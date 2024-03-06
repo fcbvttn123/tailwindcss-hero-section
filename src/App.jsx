4d8a361077e932c7fa96ef19d6d8433a00d23fa4
ngan <vutran@sheridancollege.ca>
2024-03-06 14:03:49 -0500

    Writing again

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,7 @@ import { Layout } from './components/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='product' element={<h1>Product</h1>}/>
-          <Route path='features' element={<h1>Features</h1>}/>
-        </Route>
-      </Routes>    
-    </BrowserRouter>
-  )
+  
 }
 
 export default App
